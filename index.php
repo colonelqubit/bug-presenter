a5155cd1bf799eb781ccdc4fb2f559a34daab134
Robinson Tryon <qubit@runcibility.com>
2013-10-28 19:48:55 +0000

    Present tabular information about each bug

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
 
 // Take the bugzilla data as CSV input and chop it up by Whiteboard
 // tag.
+//
+// ASSUMPTION: There will be various columns in the data including:
+//   - Bug ID
+//   - Whiteboard
 $input_file = "bugzilla-data.csv.BACKUP";
 
 $data = array();
@@ -45,6 +49,7 @@ while(!feof($in_handle)) {
   if(is_null($column_headers)) {
     $column_headers = $line_array;
 
+/*
     // We need to know which field contains the whiteboard so we can
     // grab whiteboard fields as we loop through the data.
     $whiteboard_index = array_search($WHITEBOARD_COLUMN_NAME, $line_array);
@@ -67,31 +72,42 @@ while(!feof($in_handle)) {
       print_r($line_array);
       die("Bug ID column kahhhhhhhhn...");
     }
+*/
   } else {
-    // Grab a line of bugzilla data.
-    $data[] = $line_array;
+    // Grab a line of bugzilla data and put it in a hash keyed by
+    // column header.
+    $row = array_combine($column_headers, $line_array);
 
     // Grab the whiteboard string, split it into tags, and add the
     // tags to our hash of whiteboard tags.
 
+    $tags = array();
     // NOTE: explode() won't work, as we sometimes have
     // extraneous/duplicate whitespace in the whiteboard.
     // UPDATE: Well...perhaps we should see any weird whitespace problems?
-    $tags = explode(" ", $line_array[$whiteboard_index]);
+    if(!empty($row["Whiteboard"])) {
+      $tags = explode(" ", $row["Whiteboard"]);
+    }
     //$tags = preg_split('/\s+/', $line_array[$whiteboard_index]);
 
     foreach($tags as $tag) {
-      // Throw out empty strings.
-      if(!empty($tag)) {
-        // Add this bug # to the list.
-        $whiteboard_tags[$tag] []= $line_array[$bug_id_index];
-      }
+      // Add this bug # to the list.
+      $whiteboard_tags[$tag] []= $row["Bug ID"];
     }
+
+    // Add the bug data to our general hash, keyed by Bug ID.
+    // The whiteboard data is more convenient when stored in its own
+    // array, so we'll use that
+    $row["Whiteboard"] = $tags;
+    $data[$row["Bug ID"]] = $row;
   }
 }
 fclose($in_handle);
 
-print "Bugzilla data is here.<br>\n";
+print "<h1>Bug Presenter:</h1>\n";
+print "<h3>LibreOffice Bugs organized by <a href=\"https://wiki.documentfoundation.org/QA/Bugzilla/Fields/Whiteboard\">whiteboard</a> tags.</h3>\n";
+
+print "<br><br><br>\n";
 
 print "Headers are...<br>\n";
 print "<ul>\n";
@@ -102,22 +118,49 @@ print "</ul>\n";
 
 print "<hr>\n";
 print "Whiteboard tags are...<br>\n";
-print "<table>\n";
 
 // Order whiteboard tags alphabetically.
 ksort($whiteboard_tags);
 
+// Fields to print out for each bug.
+$fields = array("Bug ID", "Component", "Status", "Summary", "Whiteboard");
+
 foreach($whiteboard_tags as $name => $id_array) {
-  print "<tr><th>$name</th><td>\n";
-  foreach($id_array as $id) {
-    print "<a href=\"$bug_show_url$id\">$id</a> \n";
+  print "<h2 id=\"$name\">$name</h2>\n";
+
+  print "<table>\n";
+  print "  <tr>\n";
+  foreach($fields as $field) {
+    print "    <th>$field</th>\n";
   }
-  print "</td></tr>\n";
+  print "  </tr>\n";
 
-}
+  foreach($id_array as $id) {
+    print "  <tr>\n";
+
+    // Bug ID
+    print "    <td><a href=\"$bug_show_url$id\">$id</a></td>\n";
+
+    // Component
+    print "    <td>{$data[$id]['Component']}</td>\n";
+
+    // Status
+    print "    <td>{$data[$id]['Status']}</td>\n";
+
+    // Summary
+    print "    <td>{$data[$id]['Summary']}</td>\n";
+
+    // Whiteboard
+    print "    <td>\n";
+    foreach($data[$id]['Whiteboard'] as $tag) {
+      print "      <a href=\"#$tag\">$tag</a>\n";
+    }
+    print "    </td>\n";
+    print "  </tr>\n";
+  }
+
 print "</table>\n";
-
-
+}
 
 
 // Create an alphabetized list of unique values listed in the
